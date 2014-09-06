cc857c154c30b43c9d6ed28add2f465f96fc30c8
iguto <clc.iguto@gmail.com>
2014-09-07 03:54:58 +0900

    盤面の保持を配列の配列へ変更
    行の毎に取り出しての操作ができなかったため

--- test/Board.cs
+++ test/Board.cs
@@ -7,45 +7,51 @@ using System.Threading.Tasks;
 namespace test {
   class Board {
     public int Size { get; set; }
-    private Stone[,] cells;
+    private Stone[][] cells;
     // constructor
     public Board(int size) {
       this.Size = size;
-      this.cells = new Stone[size, size];
+      this.cells = new Stone[this.Size][];
 
       CreateEmptyCells();
       PlaceStonesAtInitialize();
     }
 
     private void PlaceStonesAtInitialize() {
-      this.cells[this.Size / 2, this.Size / 2] = new Stone(Stone.Type.Black);
-      this.cells[this.Size / 2 - 1, this.Size / 2 - 1] = new Stone(Stone.Type.Black);
-      this.cells[this.Size / 2 - 1, this.Size / 2] = new Stone(Stone.Type.White);
-      this.cells[this.Size / 2, this.Size / 2 - 1] = new Stone(Stone.Type.White);
+      this.cells[this.Size / 2][this.Size / 2] = new Stone(Stone.Type.Black);
+      this.cells[this.Size / 2 - 1][this.Size / 2 - 1] = new Stone(Stone.Type.Black);
+      this.cells[this.Size / 2 - 1][this.Size / 2] = new Stone(Stone.Type.White);
+      this.cells[this.Size / 2][this.Size / 2 - 1] = new Stone(Stone.Type.White);
     }
 
     private void CreateEmptyCells() {
-      for (int i = 0; i < this.Size; i++) {
-        for (int j = 0; j < this.Size; j++) {
-          this.cells[i, j] = new Stone();
+      for (int i = 0; i < this.cells.Length; i++) {
+        Stone[] row = new Stone[this.Size];
+        for (int j = 0; j < row.Length; j++) {
+          row[j] = new Stone(Stone.Type.Empty);
         }
+        cells[i] = row;
       }
     }
 
     public void Display() {
       DisplayRowLine();
-      for (int i = 0; i < this.Size; i++) {
-        Console.Write("｜");
-        for (int j = 0; j < this.Size; j++) {
-          Console.Write(this.cells[i, j].ConsoleFormat() + "|");
-        }
-        Console.WriteLine();
-        DisplayRowLine();
+      foreach (var row in this.cells) {
+        DisplayStoneRow(row);
       }
     }
 
+    private void DisplayStoneRow(Stone[] stones) {
+      Console.Write("｜");
+      foreach (var stone in stones) {
+        Console.Write(stone.ConsoleFormat() + "｜");
+      }
+      Console.WriteLine();
+      DisplayRowLine();
+    }
+
     private void DisplayRowLine() {
-      Console.WriteLine(new String('-', this.Size * 3 + 2));
+      Console.WriteLine(new String('-', this.Size * 4 + 2));
     }
   }
 }
