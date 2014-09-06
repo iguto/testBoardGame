e7bfe34bfa202820a49f77c42af95d7c07dfd20e
iguto <clc.iguto@gmail.com>
2014-09-07 01:53:16 +0900

    init

---
+++ test/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+  class Board
+  {
+    public int Size { get; set; }
+    // constructor
+    public Board(int size)
+    {
+      this.Size = size;
+      Console.WriteLine("[Board] created");
+      Console.WriteLine("  board Size:" + this.Size);
+    }
+    public void Display() {
+      Console.WriteLine("[Board]");
+    }
+  }
+}
---
+++ test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Board board = new Board(5);
+          Console.WriteLine("[Program]: board:" + board.Size);
+          Console.ReadKey();
+        }
+    }
+}
